d8752fb5f3c7026afe7f504fce7fe29300d5b2b0
levos-snr <lewisodero27@gmail.com>
2023-02-28 00:01:41 +0300

    commit

---
+++ assets/js/contact.js
@@ -0,0 +1,9 @@
+/*===== DRAG and DROP =====*/
+const dropItems = document.getElementById('drop-items')
+
+new Sortable(dropItems, {
+    animation: 350,
+    chosenClass: "sortable-chosen",
+    dragClass: "sortable-drag"
+});
+
